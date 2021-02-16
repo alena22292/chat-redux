3685634f5757eea456d27b2f24331a07cb7feafb
Alena Gritsiuk <alena22292@mail.ru>
2021-02-16 23:03:52 +0100

    Add a background image via url

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,12 +4,15 @@ import ChannelList from '../containers/channel_list';
 
 const App = (props) => {
   return (
+
+    <div className="back-style">
     <div className="messaging-wrapper">
       <div className="logo-container">
         <img className="messaging-logo" src="../../assets/images/logo.svg" alt="logo" />
       </div>
       <ChannelList channelFromParams={props.match.params.channel} />
       <MessageList channelFromParams={props.match.params.channel} />
+    </div>
     </div>
   );
 };
